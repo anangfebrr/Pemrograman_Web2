248546771cfd01ce42772faaafddc4b3ba5cffe2
Anang Febryan <anangfebrian818@gmail.com>
2023-03-28 13:15:49 +0700

    tugas 5

--- praktikum5/form_pelanggan.php
+++ praktikum5/form_pelanggan.php
@@ -1,7 +1,7 @@
 <?php 
 require_once 'dbkoneksi.php';
-if (isset($_GET['idedit'])) {
-  $idedit = $_GET['idedit'];
+if (isset($_GET["idedit"])) {
+  $idedit = $_GET["idedit"];
   $query = "SELECT * FROM pelanggan WHERE id = '$idedit'";
   $sql = $dbh->query($query);
   $row = $sql->fetch();
@@ -38,7 +38,7 @@ if (isset($_GET['idedit'])) {
           </div>
         </div> 
         <input id="nama" name="nama" type="text" class="form-control" 
-        value="<?php if (isset($_GET["idedit"])) {echo $row["nama"];}?>">
+        value="<?php if (isset($_GET["idedit"])) {echo $row["nama"];} ?>">
       </div>
     </div>
   </div>
@@ -52,9 +52,9 @@ if (isset($_GET['idedit'])) {
           </div>
         </div> 
         <input id="jk" name="jk" 
-        value="L" type="radio" class="form-control" <?php if(isset($_GET["idedit"])) {if($row["jk"] == "L"){echo "checked";}}?>>Laki - Laki
-        <input id="jk" name="jk"
-        value="P" type="radio" class="form-control" <?php if(isset($_GET["idedit"])) {if($row["jk"] == "P"){echo "checked";}}?>>Perempuan
+        value="L" type="radio" class="form-control" <?php if(isset($_GET["idedit"])) {if($row["jk"] == "L"){echo "checked";}} ?>>Laki - Laki
+        <input id="jk" name="jk" 
+        value="P" type="radio" class="form-control" <?php if(isset($_GET["idedit"])) {if($row["jk"] == "P"){echo "checked";}} ?>>Perempuan
       </div>
     </div>
   </div>
@@ -67,7 +67,7 @@ if (isset($_GET['idedit'])) {
             <i class="fa fa-arrow-circle-up"></i>
           </div>
         </div> 
-        <input id="tmp_lahir" name="tmp_lahir" value="<?php if (isset($_GET["idedit"])) {echo $row["tmp_lahir"];}?>"
+        <input id="tmp_lahir" name="tmp_lahir" value="<?php if (isset($_GET["idedit"])) {echo $row["tmp_lahir"];} ?>"
         type="text" class="form-control">
       </div>
     </div>
@@ -81,14 +81,14 @@ if (isset($_GET['idedit'])) {
             <i class="fa fa-arrow-circle-right"></i>
           </div>
         </div> 
-        <input id="tgl_lahir" name="tgl_lahir"
-        value="<?php if (isset($_GET["idedit"])) {echo $row["tgl_lahir"];}?>
+        <input id="tgl_lahir" name="tgl_lahir" 
+        value="<?php if (isset($_GET["idedit"])) {echo $row["tgl_lahir"];} ?>"
         type="date" class="form-control">
       </div>
     </div>
   </div>
-    <div class="form-group row">
-    <label for="min_stok" class="col-4 col-form-label">E-mail</label> 
+  <div class="form-group row">
+    <label for="min_stok" class="col-4 col-form-label">email</label> 
     <div class="col-8">
       <div class="input-group">
         <div class="input-group-prepend">
@@ -97,7 +97,7 @@ if (isset($_GET['idedit'])) {
           </div>
         </div> 
         <input id="email" name="email" 
-        value="<?php if (isset($_GET["idedit"])) {echo $row["email"];}?>
+        value="<?php if (isset($_GET["idedit"])) {echo $row["email"];} ?>"
         type="email" class="form-control">
       </div>
     </div>
@@ -110,16 +110,16 @@ if (isset($_GET['idedit'])) {
             $rsjenis = $dbh->query($sqljenis);
         ?>
       <select id="kartu_id" name="kartu_id" class="custom-select">
-        <?php
-        if(isset($_GET["idedit"])){
-          $id_kartu = $row["kartu_id"];
-          $query2 = "SELECT * FROM kartu WHERE id = '$id_kartu'";
-          $sql2 = $dbh->query($query2);
-          $row2 = $sql2->fetch();
-          echo '<option value="'.$row2["id"].'">'.$row2["nama"].'</option>';
-        }
-          foreach($rsjenis as $rowjenis){
-        ?>
+          <?php 
+            if (isset($_GET["idedit"])) {
+              $id_kartu = $row["kartu_id"];
+              $query2 = "SELECT * FROM kartu WHERE id = '$id_kartu'";
+              $sql2 = $dbh->query($query2);
+              $row2 = $sql2->fetch();
+              echo '<option value="'.$row2['id'].'">'.$row2['nama'].'</option>';
+            }
+            foreach($rsjenis as $rowjenis){
+         ?>
             <option value="<?=$rowjenis['id']?>"><?=$rowjenis['nama']?></option>
          <?php
             }
@@ -135,7 +135,7 @@ if (isset($_GET['idedit'])) {
   <div class="form-group row">
     <div class="offset-4 col-8">
       <input type="submit" name="proses" type="submit" 
-      class="btn btn-primary" value="<?php if (isset($_GET["idedit"])) {echo "update";} else{echo "simpan";}?>"/>
+      class="btn btn-primary" value="<?php if (isset($_GET["idedit"])) {echo "Update";} else{echo "Simpan";} ?>"/>
     </div>
   </div>
 </form>
